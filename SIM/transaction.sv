a8dc9b2c44ccc6c39f54beb875335f505a091a27
Sawera Zahra <98219102+engr-zahra@users.noreply.github.com>
2022-04-22 01:26:46 +0500

    Update transaction.sv

--- SIM/transaction.sv
+++ SIM/transaction.sv
@@ -23,4 +23,15 @@ class transaction;
     if(HWRITE == 1'b0) $displayh("\t HWRITE = %0d",HWRITE);
     $display("-----------------------------------------");
  endfunction
+ 
+ constraint random { 
+		HADDR % 4 == 0 && HADDR < 8'd256;
+		}
+constraint HSEL_select{
+			HSEL == 1;
+		}
+
+constraint customized 	{
+			HADDR == HADDR_SIZE;
+	}
 endclass : transaction

0720f8b6e8fb964496b0bea706152b3187bebc3a
Sawera Zahra <98219102+engr-zahra@users.noreply.github.com>
2022-04-21 23:11:15 +0500

    Update driver.sv

--- SIM/driver.sv
+++ SIM/driver.sv
@@ -1,62 +1,90 @@
+import ahb3lite_pkg::*;
 `include "generator.sv"
 class driver;
-  virtual memory inf_d;
-  
-  mailbox gen2drv;
+  virtual ahbif.master dif; // interface
+  mailbox #(transaction) gen2drv; // mailbox
+  int no_trans; // number of transactions
 
-  function new(virtual memory inf_d,mailbox gen2drv);
+  function new(virtual ahbif.master dif,mailbox gen2drv);
      begin
-       this.inf_d = inf_d;
+       this.dif     = dif;
        this.gen2drv = gen2drv;
      end
   endfunction 
   
   task rst; // reset
-    wait(!inf_d.HRESET) // active low state
+    wait(!dif.HRESET) // active low state
       begin
-      $display("Reset state is low");
-      inf_d.HADDR <= 0; 
-      inf_d.HSIZE <= 0;                   
-      inf_d.HREADY <= 0;
+      $display("HRESETn is in high state");
+      dif.HADDR  <= 0; 
+      dif.HSIZE  <= 0;                   
+      dif.HREADY <= 1;
       end
-    wait(inf_d.HRESET)
-      $display("Reset state is high");
-  endtask
+    wait(dif.HRESET)
+      $display("HRESETn is in low state");
+  endtask : rst
  
   task read_send; // read operation
-    while (inf_d.HRESET)
-      forever begin
-        @(posedge inf_d.HCLK);
-        inf_d.HREADY <= 1;
-        inf_d.HWRITE <= 0;
-        inf_d.HADDR <= tr.HADRR;
-        @(posedge inf_d.HCLK);
-        inf_d.HRDATA <= tr.HWDATA;
-      end
+    begin 
+        transaction tr; 
+	gen2drv.get(tr);
+	@(posedge dif.HCLK);
+        dif.HREADY  <= 1'b1;
+        dif.HADDR   <= tr.HADDR;
+        dif.HWRITE  <= 1'b0;
+        @(posedge dif.HCLK)
+        dif.HWRITE  <= tr.HWRITE;
+        @(posedge dif.HCLK);
+        tr.HRDATA   = dif.HWDATA;
+        $display("\t THe data [HWDATA] = %0h \t is read from address [HADDR] = %0h",tr.HRDATA,tr.HADDR);
+	@(posedge dif.HCLK);
+        end
   endtask
 
   task write_send; // write operation
-    while (inf_d.HRESET)
-      forever begin
-        @(posedge inf_d.HCLK);
-        inf_d.HREADY <= 1;
-        inf_d.HWRITE <= 1;
-        inf_d.HADDR <= tr.HADRR;
-        @(posedge inf_d.HCLK);
-        inf_d.HWDATA <= tr.HWDATA;
+    begin
+        transaction tr;
+        gen2drv.get(tr);
+        @(posedge dif.HCLK);
+        dif.HREADY  <= 1'b1;
+        dif.HADDR   <= tr.HADDR;
+        dif.HWRITE  <= 1'b1;
+        @(posedge dif.HCLK);
+        dif.HWRITE  <= tr.HWRITE;
+        dif.HWDATA  <= tr.HWDATA;
+        $display("\t THe data [HWDATA] = %0h \t is written on address [HADDR] = %0h",tr.HWDATA,tr.HADDR);
+        @(posedge dif.HCLK);
       end
   endtask
 
-    task run; // main task to run
-      transaction tr;
-      gen2drv.get(tr);
-      @(posedge inf_d.HCLK);
-      if (tr.HWRITE == 1);
-      write_send;
-      $display("Write transaction");
-      elseif (tr.HWRITE == 0);
-      read_send;
-      $display("Read transaction");
-      
-    endtask
+  task send; // main task to run
+    forever begin 
+        $display("Driver has recieved transfer : %0d ",no_trans);
+        if (tr.HWRITE == 1'b0) begin
+        read_send;
+	$display("          Read transaction             ");
+        end
+        if (tr.HWRITE == 1'b1) begin
+        write_send;
+        $display("          Write transaction            ");
+	end
+        dif.HRESP = HRESP_OKAY;
+        dif.HREADY = 1'b1; // consider is hreadyout
+     end
+     $display("------------------------------------------");
+     no_trans++; // next transaction   
+  endtask : send
+  
+  task drive;
+    fork
+      begin
+        wait(!dif.HRESET); //Wait for reset
+      end
+      begin
+        forever
+          send(); // main driver task
+        end
+    join_any
+    disable fork;
+  endtask : drive
 endclass: driver
